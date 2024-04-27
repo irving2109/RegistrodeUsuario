c248db2e4db48c4810c04b1034a345c5045c611f
Irving Gutierez <108704594+irving2109@users.noreply.github.com>
2024-04-26 19:07:48 -0500

    class

---
+++ src/RegistrodeUsuario.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegistrodeUsuario {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
